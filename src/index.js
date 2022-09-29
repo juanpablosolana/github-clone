198b887865399058f58d1cb6d70714a8eee7d516
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2022-09-28 19:42:41 -0500

    profile component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStylesStyled from './components/Global-styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <>
+    <GlobalStylesStyled />
     <App />
+    </>
   </React.StrictMode>
 );
 
