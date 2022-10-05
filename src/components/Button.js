4bb7f17f5f4a6e044571639cee37b42af6ceca45
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2022-10-04 20:28:18 -0500

    modal

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,23 +2,23 @@ import { isValidElement } from 'react'
 import styled from 'styled-components'
 
 const ButtonStyled = styled.button`
- font: var(--button);
- border-radius: .5rem;
- padding-block: .25rem;
- background: var(--buttonBG);
- color: var(--white);
- border: 1px solid var(--grey);
- min-inline-size: 135px;
- gap: .5rem;
- display: inline-flex;
- align-items: center;
- justify-content: center;
- cursor: pointer;
- text-decoration: none !important;
- &:hover {
-   background: var(--white);
-   color: var(--button);
- }
+font: var(--button);
+border-radius: .5rem;
+padding-block: .25rem;
+background: var(--buttonBG);
+color: var(--white);
+border: 1px solid var(--grey);
+min-inline-size: 135px;
+gap: .5rem;
+display: inline-flex;
+align-items: center;
+justify-content: center;
+cursor: pointer;
+text-decoration: none !important;
+&:hover {
+  background: var(--white);
+  color: var(--button);
+}
 `
 
 function Button({ text, link, className, icon }) {
---
+++ src/modal.js
@@ -0,0 +1,52 @@
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Overlay from './Overlay'
+import styled from 'styled-components'
+import { ButtonContrast } from './components/Button'
+import InputText from './components/Input-text'
+
+const ModalContentStyled = styled.form`
+  background: var(--bg);
+  color: var(--white);
+  padding: 1.5rem;
+  border-radius: .5rem;
+  position: fixed;
+  inset-block-start: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  inset-inline-start: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  inline-size: 24rem;
+  .title {
+    font: var(--headline2-semi-bold);
+    margin: 0;
+  }
+`
+
+function ModalContent() {
+  const form = useRef(null)
+  const navigator = useNavigate()
+  console.log({ form })
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log({ form })
+
+    const formData = new FormData(form.current)
+    console.log(formData.get('username'))
+    navigator(`/${formData.get('username')}`)
+
+  }
+  return (
+    <Overlay>
+      <ModalContentStyled ref={form} action="" onSubmit={handleSubmit}>
+        <h2 className='title'>Busca a tu usuario favorito</h2>
+        <InputText type="text" autoComplete='off' name="username" placeholder="Username" />
+        <ButtonContrast text="Buscar" />
+      </ModalContentStyled>
+    </Overlay>
+  )
+}
+
+export default ModalContent
